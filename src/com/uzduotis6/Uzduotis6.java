1482cee8aa849576729a68df6a59a19924f4088f
dileta123 <dileta.darbinis@gmail.com>
2017-03-06 20:15:26 +0200

    main class commited

--- src/com/uzduotis6/Uzduotis6.java
+++ src/com/uzduotis6/Uzduotis6.java
@@ -6,16 +6,14 @@ import java.util.Scanner;
  * Created by Svecias on 3/6/2017.
  */
 public class Uzduotis6 {
-    public Uzduotis6()
-    {
+    public Uzduotis6() {
         System.out.print("Iveskite skaiciu nuo 1 iki 100");
         Scanner scanner = new Scanner(System.in);
         int sk1 = scanner.nextInt();
-        for (sk1 = 1; sk1 <= 100; sk1++) ;
+
         if (sk1 % 2 == 0) {
             System.out.print("Skaicius" + sk1 + "yra lyginis");
-        }
-        if (sk1 % 2 == 1) {
+        } else {
             System.out.print("Skaicius" + sk1 + "yra nelyginis");
         }
     }
